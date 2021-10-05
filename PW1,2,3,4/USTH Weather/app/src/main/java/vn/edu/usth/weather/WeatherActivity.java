28ad7aaf3a2a5a13404d58de0bcb529a026becfa
phamlam2k <mailam1309@gmail.com>
2021-10-05 20:59:17 +0700

    Praticial Work

---
+++ PW1,2,3,4/USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,43 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+    public static final String tag = "WeatherActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(tag, "On Starting");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(tag,"On Resuming");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(tag,"On Stopping");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(tag,"On Pausing");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(tag,"On Destroying");
+    }
+
+}
+
+
