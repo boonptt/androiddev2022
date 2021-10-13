f01ef88d45ef969ddb841551a96f51162c4b53bd
phamlam2k <mailam1309@gmail.com>
2021-10-13 09:37:21 +0700

    fix weather_act

--- PW1,2,3,4/USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ PW1,2,3,4/USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,17 +1,45 @@
 package vn.edu.usth.weather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager.widget.PagerAdapter;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class WeatherActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
     public static final String tag = "WeatherActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        Log.i(tag, "On Creating");
+
+        tabLayout = findViewById(R.id.tab_layout);
+        TabLayoutMediator TabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("tab0");
+                    break;
+                case 1:
+                    tab.setText("tab1");
+                    break;
+                case 2:
+                    tab.setText("tab2");
+                    break;
+            }
+        }).attach();
     }
+
     @Override
     protected void onStart() {
         super.onStart();
