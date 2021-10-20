08df8dc78ce463e8b8b51984aa64da5d2dfff022
phamlam2k <mailam1309@gmail.com>
2021-10-20 09:10:34 +0700

    upload Pref activity

---
+++ PW1,2,3,4/USTH Weather/app/src/main/java/vn/edu/usth/weather/PrefActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import vn.edu.usth.weather.WeatherActivity;
+
+public class PrefActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pref);
+    }
+}
